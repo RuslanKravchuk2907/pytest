5de127cc95b4ac176f1e8f79fa11c3b77f00ea88
melad <meladzeruslan164@gmail.com>
2024-10-06 02:24:17 +0300

    Exception Handling + Test Grouping

--- test/test_main.py
+++ test/test_main.py
@@ -1,25 +1,27 @@
 import pytest
-
+from contextlib import nullcontext as dos_not_raise
 from app.main import Calculator
 
+class TestCalculator:
+    @pytest.mark.parametrize(
+        "x, y, res, expectation",
+        [
+            (1, 2, 0.5, dos_not_raise),
+            (5, -1, -5, dos_not_raise),
+            (5, "-1", -5, pytest.raises(TypeError)),
+        ]
+    )
+    def test_divide(self, x, y, res, expectation):
+        with expectation:
+            assert Calculator().divide(x, y) == res
 
-@pytest.mark.parametrize(
-    "x, y, res",
-    [
-        (1, 2, 0.5),
-        (5, -1, -5),
-    ]
-)
-def test_divide(x, y, res):
-    assert Calculator().divide(x, y) == res
 
-
-@pytest.mark.parametrize(
-    "x, y, res",
-    [
-        (1, 2, 3),
-        (5, -1, 4),
-    ]
-)
-def test_add(x, y, res):
-    assert Calculator().add(x, y) == res
+    @pytest.mark.parametrize(
+        "x, y, res",
+        [
+            (1, 2, 3),
+            (5, -1, 4),
+        ]
+    )
+    def test_add(self, x, y, res):
+        assert Calculator().add(x, y) == res
