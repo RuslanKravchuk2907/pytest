5103f88669824e1b914705e6fcc31c3571b8a6ec
melad <meladzeruslan164@gmail.com>
2024-10-09 17:24:01 +0300

    Added the first request

--- test/test_main.py
+++ test/test_main.py
@@ -1,28 +1,14 @@
-import pytest
-from contextlib import nullcontext as dos_not_raise
-from app.main import Calculator
+import requests
 
-class TestCalculator:
-    @pytest.mark.parametrize(
-        "x, y, res, expectation",
-        [
-            (1, 2, 0.5, dos_not_raise),
-            (5, -1, -5, dos_not_raise),
-            (5, "-1", -5, pytest.raises(TypeError)),
-            (0, -1, -5, pytest.raises(ZeroDivisionError)),
-        ]
-    )
-    def test_divide(self, x, y, res, expectation):
-        with expectation:
-            assert Calculator().divide(x, y) == res
+from configuration import SERVICE_URL
+
+from src.enums.global_enums import GlobalErrorMessages
+
+def test_getting_posts():
+    response = requests.get(url=SERVICE_URL)
+    received_posts = response.json()
+
+    assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
+    assert len(received_posts) ==3, GlobalErrorMessages.WRONG_ELENET_COUNT.value
 
 
-    @pytest.mark.parametrize(
-        "x, y, res",
-        [
-            (1, 2, 3),
-            (5, -1, 4),
-        ]
-    )
-    def test_add(self, x, y, res):
-        assert Calculator().add(x, y) == res
